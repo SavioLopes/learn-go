903598aa1fc538a00c5b7f6a6b34e19247a7f0dc
Savio Lopes <savio@minutrade.com>
2017-11-30 14:34:44 -0200

    variables

---
+++ variables/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "reflect"
+)
+
+func main() {
+  var text = "string"
+  fmt.Println(reflect.TypeOf(text))
+
+  var integerOne = 1
+  fmt.Println(reflect.TypeOf(integerOne))
+  
+  var float = 1.1
+  fmt.Println(reflect.TypeOf(float))
+  
+  var str string = "string"
+  fmt.Println(reflect.TypeOf(str))
+
+  var integerTwo int
+  fmt.Println(integerTwo)
+  fmt.Println(reflect.TypeOf(integerTwo))
+}
